c0b38eb85b39443cbd4b8f96a00f263c0f98224a
Juan Carlos Chambi Python Dev <juan199963@gmail.com>
2022-03-22 20:34:20 -0500

    6-Clase

--- main.js
+++ main.js
@@ -16,9 +16,50 @@ function Student(name, age, cursosAprobados){
     this.name = name;
     this.age = age;
     this.cursosAprobados = cursosAprobados;
+
 };
+
+Student.prototype.aprobarCurso = function(nuevoCursito){
+    this.cursosAprobados.push(nuevoCursito);
+}
+
+
+const Juanita = new Student(
+    'Juanita Alejandra',
+    15,
+    [
+        "Cursos de Introduccion a los videojuegos",
+        "Creacion de personajes"
+    ]
+);
+
 
 // Hacer que Natalia aprueba otro curso
 
 natalia.cursosAprobados.push('Curso de Responsive Design');
 
+
+class Student2{
+    constructor({
+        name,
+        age,
+        cursosAprobados = [],
+        email,
+    }){
+        this.name = name;
+        this.age = age;
+        this.cursosAprobados = cursosAprobados;
+        this.email = email;
+    }
+
+    aprobarCurso(nuevoCursito){
+        this.cursosAprobados.push(nuevoCursito);
+    }
+};
+
+
+const miguelito = new Student2({
+    name: "Miguel",
+    age:22,
+    email: 'jcz_1999@hotmail.com'
+});
