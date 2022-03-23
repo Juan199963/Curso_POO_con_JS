0dde5a8e2fd403c78d3fdfa5f46fde7acc01a661
Juan Carlos Chambi Python Dev <juan199963@gmail.com>
2022-03-22 21:45:49 -0500

    Agregado

--- 2_EJERCICIO/main.js
+++ 2_EJERCICIO/main.js
@@ -1,65 +1,105 @@
-const juan1 ={
+// const juan1 ={
+//     name: "JuanDC",
+//     username: "juandc",
+//     points: 100,
+//     socialmedia: {
+//         twitter: "fjuandc",
+//         facebook: "fjuandc",
+//         instagram: undefined,
+//     },
+//     approvechedCourses:[
+//         "Curso de HTML",
+//         "Curso de CSS",
+//     ],
+//     learningPaths: [
+//         {
+//             name:"Escuela de Desarrollo Web",
+//             courses:[
+//                 "Curso de HTML",
+//                 "Curso de CSS",
+//                 "Curso de Responsive Design",
+//             ]
+//         },
+//         {
+//             name:"Escuela de Videojuegos",
+//             courses:[
+//                 "Curso de Unreal Engine",
+//                 "Curso de Unity",
+//             ]
+//         }
+//     ]
+// };
+
+
+// const miguelito ={
+//     name: "Miguelito",
+//     username: "miguelitofeliz",
+//     points: 100,
+//     socialmedia: {
+//         twitter: "miguelitofeliz",
+//         facebook: "miguelitofeliz",
+//         instagram: undefined,
+//     },
+//     approvechedCourses:[
+//         "Curso de DATAVIZ",
+//         "Curso de PYTHON",
+//     ],
+//     learningPaths: [
+//         {
+//             name:"Escuela de Desarrollo Web",
+//             courses:[
+//                 "Curso de HTML",
+//                 "Curso de CSS",
+//                 "Curso de Responsive Design",
+//             ]
+//         },
+//         {
+//             name:"Escuela de DataScience",
+//             courses:[
+//                 "Curso de DATAVIZ",
+//                 "Curso de PYTHON",
+//                 "Curso de Tableau",
+//             ]
+//         }
+//     ]
+// };
+
+
+class Student{
+    constructor({
+        name,
+        email,
+        username,
+        twitter = undefined,
+        instagram = undefined,
+        facebook = undefined,
+        approvedCourses = [],
+        learningPaths = [],
+    }){
+        this.name = name;
+        this.email = email;
+        this.username = username;
+        this.socialmedia = {
+            twitter,
+            instagram,
+            facebook,
+        };
+        this.approvedCourses = approvedCourses;
+        this.learningPaths = learningPaths;
+    }
+}
+
+const juan2 = new Student({
     name: "JuanDC",
     username: "juandc",
-    points: 100,
-    socialmedia: {
-        twitter: "fjuandc",
-        facebook: "fjuandc",
-        instagram: undefined,
-    },
-    approvechedCourses:[
-        "Curso de HTML",
-        "Curso de CSS",
-    ],
-    learningPaths: [
-        {
-            name:"Escuela de Desarrollo Web",
-            courses:[
-                "Curso de HTML",
-                "Curso de CSS",
-                "Curso de Responsive Design",
-            ]
-        },
-        {
-            name:"Escuela de Videojuegos",
-            courses:[
-                "Curso de Unreal Engine",
-                "Curso de Unity",
-            ]
-        }
-    ]
-};
+    email:"juan@gmail.com",
 
+})
 
-const miguelito ={
-    name: "Miguelito",
-    username: "miguelitofeliz",
-    points: 100,
-    socialmedia: {
-        twitter: "miguelitofeliz",
-        facebook: "miguelitofeliz",
-        instagram: undefined,
-    },
-    approvechedCourses:[
-        "Curso de DATAVIZ",
-        "Curso de PYTHON",
-    ],
-    learningPaths: [
-        {
-            name:"Escuela de Desarrollo Web",
-            courses:[
-                "Curso de HTML",
-                "Curso de CSS",
-                "Curso de Responsive Design",
-            ]
-        },
-        {
-            name:"Escuela de DataScience",
-            courses:[
-                "Curso de DATAVIZ",
-                "Curso de PYTHON",
-                "Curso de Tableau",
-            ]
-        }
-    ]
-};
+const miguelito = new Student({
+    name: "Miguel",
+    username: "migueldc",
+    email:"juan@gmail.com",
+    instagram:"miguelito_feliz",
+
+})
