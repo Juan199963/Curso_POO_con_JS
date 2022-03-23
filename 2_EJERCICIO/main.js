04ab1f5a6c1e00666c6e3d28cbb7e23e50906ae7
Juan Carlos Chambi Python Dev <juan199963@gmail.com>
2022-03-22 21:27:13 -0500

    Se modifico los archivos

---
+++ 2_EJERCICIO/main.js
@@ -0,0 +1,65 @@
+const juan1 ={
+    name: "JuanDC",
+    username: "juandc",
+    points: 100,
+    socialmedia: {
+        twitter: "fjuandc",
+        facebook: "fjuandc",
+        instagram: undefined,
+    },
+    approvechedCourses:[
+        "Curso de HTML",
+        "Curso de CSS",
+    ],
+    learningPaths: [
+        {
+            name:"Escuela de Desarrollo Web",
+            courses:[
+                "Curso de HTML",
+                "Curso de CSS",
+                "Curso de Responsive Design",
+            ]
+        },
+        {
+            name:"Escuela de Videojuegos",
+            courses:[
+                "Curso de Unreal Engine",
+                "Curso de Unity",
+            ]
+        }
+    ]
+};
+
+
+const miguelito ={
+    name: "Miguelito",
+    username: "miguelitofeliz",
+    points: 100,
+    socialmedia: {
+        twitter: "miguelitofeliz",
+        facebook: "miguelitofeliz",
+        instagram: undefined,
+    },
+    approvechedCourses:[
+        "Curso de DATAVIZ",
+        "Curso de PYTHON",
+    ],
+    learningPaths: [
+        {
+            name:"Escuela de Desarrollo Web",
+            courses:[
+                "Curso de HTML",
+                "Curso de CSS",
+                "Curso de Responsive Design",
+            ]
+        },
+        {
+            name:"Escuela de DataScience",
+            courses:[
+                "Curso de DATAVIZ",
+                "Curso de PYTHON",
+                "Curso de Tableau",
+            ]
+        }
+    ]
+};
